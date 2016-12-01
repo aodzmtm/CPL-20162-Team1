184fd808090bce3c7d1b2d284cbf3ed64de3315e
aodzmtm <aodzmtm@naver.com>
2016-12-01 12:39:39 +0900

    Instructions about Application has added.

--- SafeLight/app/src/main/java/com/example/safelight/MainActivity.java
+++ SafeLight/app/src/main/java/com/example/safelight/MainActivity.java
@@ -130,10 +130,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        // 처음 실행시 먼저 Map fragment 정보를 layout 파일로부터 읽어들인다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // 로컬 파일 읽어오기
+        // 다음으로 로컬 파일에 저장돼있던 보안등고 마커 정보 데이터를 읽어온다.
         loadTxt();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -187,6 +190,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         killService();
     }
 
+    // 뒤로가기 두 번 클릭시 종료하는 기능
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
@@ -205,30 +209,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(MainActivity.this, SettingActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
     Button.OnClickListener mClickListener = new View.OnClickListener()
     {
         @Override
         public void onClick(View v) {
             switch(v.getId())
             {
-                case R.id.btn_Scan:
+                case R.id.btn_Scan: // 비컨 스캔
                     ProgressDial(v.getId());
                     scanBLE();
                     break;
@@ -242,11 +229,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                     getInfo();
                     print_updatedMarker(mMap);
                     break;
-                case R.id.btn_Route://신고된길만표시
+                case R.id.btn_Route://신고된길, 보안등 필터링
                     mMap.clear();
                     print_onlyMarker(mMap);
                     break;
-                case R.id.btn_Report://신고
+                case R.id.btn_Report://신고 기능
                     // GPS 통신 관련 매니저 생성
                     LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                     alertShow(lm);
@@ -308,7 +295,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                         // Action for 'Yes' Button
                         try{
                             //ProgressDial(id);
-                            // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+                            // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기
                             lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
                                     100, // 통지사이의 최소 시간간격 (miliSecond)
                                     1, // 통지사이의 최소 변경거리 (m)
@@ -339,6 +326,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+    // 최초 앱 실행시 로컬에서 불러온 정보를 토대로 구글맵 상에 보안등, 마커를 출력한다.
     public void print_Marker(final GoogleMap map) {
 
         new Handler(Looper.getMainLooper()).post(new Runnable() {
@@ -347,7 +335,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 for (int i = 0; i < rows_sec; i++) {
                     add_SecLight(map, markers_sec[i][1], markers_sec[i][2], markers_sec[i][3]);
                 }
-
                 // 마커
                 for (int i = 0; i < rows_mark; i++) {
                     add_Marker(map, markers[i][1], markers[i][2], markers[i][3]);
@@ -357,6 +344,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         });
     }
+
+    // 서버와 동기화를 통해 얻어온 정보로 구글맵 상에 마커와 보안등을 새로 출력한다.
     public void print_updatedMarker(final GoogleMap map) {
 
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
@@ -377,6 +366,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }, 3500);
     }
 
+    // 필터링 기능을 작동했을때 마커, 보안등의 정보를 번갈아 출력한다.
     public void print_onlyMarker(final GoogleMap map) {
 
         new Handler(Looper.getMainLooper()).post(new Runnable() {
@@ -401,7 +391,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    // 보안등 형태별 마커
+    // 보안등 마커(아이콘) 출력 부분
+    // add_SecLight에는 인자로 x, y 좌표와 상태정보가 넘어간다. 상태 정보에 따라 보안등 아이콘 색깔이 달라진다.
     public void add_SecLight(GoogleMap map, double x, double y, double num){
 
         LatLng obj = new LatLng(x,y);
@@ -427,6 +418,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         map.addMarker(new MarkerOptions().position(obj)
                 .icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
     }
+
+    // 신고된 길 마커 출력 부분
+    // add_Marker에는 인자로 x, y 좌표와 count 값이 넘어간다. count 값에 따라 지도상에 찍을 원 형태의 마커 반지름이 달라진다.
     public void add_Marker(GoogleMap map, double x, double y, double count){
 
         if(count>0) {
@@ -454,7 +448,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    //잠깐 로딩 다이얼 띄우기
+    // 로딩이 필요한 경우 Progress Dialog를 출력한다.
     public void ProgressDial(int id){
         final ProgressDialog dialog;
 
@@ -471,17 +465,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 break;
             case R.id.btn_Refresh:
                 dialog = ProgressDialog.show(MainActivity.this, "", "데이터 동기화중입니다. 잠시 기다려주세요", true);
-                dialog.show();
-                mHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog.dismiss();
-                    }
-                }, 3500);//1000=1s
-                break;
-            // REPORT에서 신고 확인 눌렀을 경우
-            case -1:
-                dialog = ProgressDialog.show(MainActivity.this, "", "신고중입니다. 잠시 기다려주세요", true);
                 dialog.show();
                 mHandler.postDelayed(new Runnable() {
                     @Override
@@ -517,6 +500,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             mHandler.sendEmptyMessageDelayed(0, SCAN_PERIOD);
         }
     };
+
     // 소켓 통신 담당 핸들러
     private Handler mSocketHandler = new Handler()
     {
@@ -525,27 +509,27 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             String handlerText = (String)msg.obj;
             String bID = handlerText.substring(14,20);
 
-
             switch(msg.what)
             {
                 case MSG_RECEIVED_ACK:
-                    if(!mStatePacket.check_ID(bID))          //보안등 ID가 리스트에 없다 => 보안등 ID요청 ACK
+                    if(!mStatePacket.check_ID(bID))       // 최초에 앱에서 서버로 보안등 ID 요청을 한 경우 서버로부터 ID가 응답으로 온다.
                     {
-                        mStatePacket.set_ID(bID);
-                        // 서버로 비컨의 상태를 전송한다.
-                        sendSocketMsg('E', bID, bDate, bState);
+                        mStatePacket.set_ID(bID);         // 앱에서는 서버에서 받은 ID 값을 등록한다.
+                        sendSocketMsg('E', bID, bDate, bState); // 등록한 ID 정보와 함께 스캔한 보안등 상태 정보를 다시 서버로 보낸다.
                     }
-                    else if(mStatePacket.check_ID(bID))  //상태 정보에대한 ACK
+                    else if(mStatePacket.check_ID(bID))  // 앱에서 상태 정보를 서버로 넘겼을때는 응답 형태가 ID가 아닌 ACK로 오게 된다.
                     {
-                        mStatePacket.remove_ID(bID);
+                        mStatePacket.remove_ID(bID);      // 이 경우에는 등록했던 ID를 삭제하고 통신을 마친다.
                     }
                     break;
             }
         }
     };
+
+    // 블루투스 환경 설정 및, 비컨 스캔 함수 호출
     public void scanBLE() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // 폰이 BLE 지원하지 않는 경우
+        // 폰이 BLE 지원하지 않는 경우 이용할 수 없다.
         if(mBluetoothAdapter == null){                          //if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
@@ -563,10 +547,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         if(isMarsh)
         {
-            Permissioncheck();
+            Permissioncheck();      // SDK 버전에 따라 권한 체크를 해줘야 한다.
         }
-        scanLeDevice(true);
+        scanLeDevice(true);         // 블루투스 환경 설정이 끝났으면 비컨 스캔을 실시한다.
     }
+
+
+    // 비컨 스캔 함수
     public void scanLeDevice(final boolean enable) {
         if (enable) {
             // Stops scanning after a pre-defined scan period.
@@ -600,6 +587,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             };
 
+
+    // 비컨 스캔을 통해 얻은 데이터를 파싱하고 서버로 전송한다.
     private void getBleDeviceInfoFromLeScan(BluetoothDevice device, int rssi, byte[] scanRecord)
     {
         String devName;
@@ -631,39 +620,38 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 scanRecordAsHex.substring(38, 50));
 
         // 현재시간 받아오기
-        SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy:MMddHHmmss");
+        SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy:ss");
         Date currentYear = new Date();
         String mDate = mSimpleDateFormat.format ( currentYear );
 
         // 년도
         String bYear = mDate.substring(2,4);
         // 월
-        //int tempMonth = BleUtils.hex2dec(scanRecordAsHex.substring(51,52));
-        //String bMonth = mBleUtils.dec2str(tempMonth);
+        int tempMonth = BleUtils.hex2dec(scanRecordAsHex.substring(51,52));
+        String bMonth = mBleUtils.dec2str(tempMonth);
         // 일
-        //int tempDay= BleUtils.hex2dec(scanRecordAsHex.substring(53,54));
-        //String bDay = mBleUtils.dec2str(tempDay);
+        int tempDay= BleUtils.hex2dec(scanRecordAsHex.substring(53,54));
+        String bDay = mBleUtils.dec2str(tempDay);
         // 시
-        //int tempHour = BleUtils.hex2dec(scanRecordAsHex.substring(55,56));
-        //String bHour = mBleUtils.dec2str(tempHour);
+        int tempHour = BleUtils.hex2dec(scanRecordAsHex.substring(55,56));
+        String bHour = mBleUtils.dec2str(tempHour);
         // 분
-        //int tempMin = BleUtils.hex2dec(scanRecordAsHex.substring(57,58));
-        //String bMin = mBleUtils.dec2str(tempMin);
+        int tempMin = BleUtils.hex2dec(scanRecordAsHex.substring(57,58));
+        String bMin = mBleUtils.dec2str(tempMin);
         // 초
-        //String bSec = mDate.substring(5,7);
+        String bSec = mDate.substring(5,7);
 
         // 파싱한 최종 날짜 및 시간
-        //bDate = bYear + bMonth + bDay + bHour + bMin + bSec;
-        bDate = bYear+ mDate.substring(5,15);
+        bDate = bYear + bMonth + bDay + bHour + bMin + bSec;
 
-        // 비컨 상태정보
+        // 비컨 상태정보 파싱
         int tempState1 = mBleUtils.hex2dec(scanRecordAsHex.substring(58,60));
         bState = mBleUtils.parseState(mBleUtils.byte2bitset((byte)tempState1));
 
         // 파싱한 상태정보 값 설정
         mStatePacket.set_State(bState);
 
-        sendSocketMsg('R', bAddress, bDate, null);            // 메세지 전송
+        sendSocketMsg('R', bAddress, bDate, null);            // 서버로 파싱한 정보를 전송한다. cmd는 'R'
 
     }
     public String getBeaconUuid(SharedPreferences pref)
@@ -690,6 +678,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+
+    // 동기화시 http 연결을 통해 json 타입의 비컨 및 신고지역 정보를 받아온다.
     public void getInfo(){
         //http post요청 코드
         Thread thread = new Thread() {
@@ -700,6 +690,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
             public void run() {
 
+                // http 연결 부분
                 String urlString = "http://"+IP+":8080/light_web/mobileLampData.do";
                 String urlString2 = "http://"+IP+":8080/light_web/mobileDangerData.do";
                 //adding some data to send along with the request to the server
@@ -764,10 +755,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                         markers_sec_server = new double[lampInfoArray.size()][4];
                         markers_server = new double[markerInfoArray.size()][4];
 
-                        // 보안등 정보 입력
+                        // 보안등 정보 입력, 보안등은 [][0]번 값이 0이다.
                         for (int i = 0; i < lampInfoArray.size(); i++) {
                             // 배열 안에 있는것도 JSON형식 이기 때문에 JSON Object 로 추출
                             JSONObject lampVoObject = (JSONObject) lampInfoArray.get(i);
+
+                            // 보안등의 상태는 [][3]번 값을 구분하는데, 0이면 정상, 1이면 완전 고장, 2면 부분 고장인 상태다.
 
                             // 정상인 보안등일 경우
                             if(Integer.valueOf(lampVoObject.get("power_off").toString())==0
@@ -803,7 +796,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }
 
-                        // 마커 정보 입력
+                        // 마커 정보 입력, 마커는 [][0]번 값이 1이다.
+                        // [][3]번 값은 신고된 만큼의 count 값이다.
                         for (int i = 0; i < markerInfoArray.size(); i++) {
                             JSONObject markerObject = (JSONObject) markerInfoArray.get(i);
                             markers_server[i][0] = 1.0;
@@ -812,7 +806,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                             markers_server[i][3] = Double.valueOf(markerObject.get("count").toString());
                         }
 
-                        // 서버에서 받아온 값 로컬에 저장.
+                        // 서버에서 받아온 값을 로컬 파일에 저장한다.
                         saveText(markers_sec_server, markers_server);
 
                     } catch (Exception e) {
@@ -831,12 +825,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         thread.start();
     }
 
-    //REPORT시 GPS 불러서 count 값 갱신
+    // REPORT 기능 수시 GPS listener를 통해 현재 좌표를 얻고,
+    // 가장 가까이에 있는 신고 지역의 좌표를 서버로 전송한다.
+    // (서버에서는 해당 좌표에 count 값을 1 증가시킨다)
     private void addCount(LocationManager lm){
         final double xtoServ, ytoServ;
         int num=0;
         double sum = Math.pow(Math.abs(x-markers[0][1]),2)+Math.pow(Math.abs(y-markers[0][2]),2);
         double temp = 0.0;
+
+        // 가장 가까이에 있는 신고 지역 좌표 구하는 부분
         for(int i=0;i<markers.length;i++){
             temp = Math.pow(Math.abs(x-markers[i][1]),2)+Math.pow(Math.abs(y-markers[i][2]),2);
             if(sum > temp){
@@ -854,7 +852,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }catch(SecurityException ex){
         }
 
-        //http post요청 코드
+        // 좌표 정보를 넘겨주기 위해 서버와 HTTP 연결을 한다.
         Thread thread = new Thread() {
 
             @Override
@@ -889,7 +887,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    // GPS 좌표 변화 감지 리스너
+    // GPS 좌표 변화 감지 리스너 ( REPORT 기능을 수행할 때만 작동한다 )
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
             //여기서 위치값이 갱신되면 이벤트가 발생한다.
@@ -917,6 +915,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     //   public static final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
 
+    // SDK 버전에 따라 권한 체크가 필요한 경우가 있다.
     public void Permissioncheck(){
         if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
 
@@ -960,11 +959,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    // 기존에 로컬에 저장된 정보 불러오기
+    // 기존에 로컬에 저장된 보안등 및 마커 정보 불러오기
     public void loadTxt(){
         Thread thread = new Thread(){
             public void run(){
                 try{
+                    // 파일 이름은 lamp.txt
                     FileInputStream fis = openFileInput("lamp.txt");
                     byte[] data = new byte[fis.available()];
                     while(fis.read(data) != -1){
@@ -983,13 +983,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                     fis.close();
 
-                    // 파일에 저장된 갯수만큼 markers 배열 생성
+                    // 파일에 저장된 갯수만큼 보안등과 신고지역 배열 생성
+                    // 보안등은 markers_sec
+                    // 신고지역은 markers
                     markers_sec = new double[rows_sec][4];
                     markers = new double[rows_mark][4];
 
                     rows_sec=0; rows_mark=0;
 
-                    // 파일 객체 재생성 (bufferedreader에서는 seek 불가능)
+                    // BufferedReader에서 seek 기능이 없으므로, 배열 동적 할당 후에
+                    // 파일을 다시 불러와서 값을 생성했던 배열에 값을 채워넣는다.
                     FileInputStream fis2 = openFileInput("lamp.txt");
                     byte[] data2 = new byte[fis2.available()];
                     while(fis2.read(data2) != -1){
@@ -1034,7 +1037,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         thread.start();
     }
 
-    // 동기화된 정보 파일에 덮어쓰기
+    // 동기화된 정보를 기존 로컬 파일(lamp.txt) 에 덮어쓰기
     public void saveText(final double[][] array_light, final double[][] array_marker){
         Thread thread = new Thread(){
             public void run(){
